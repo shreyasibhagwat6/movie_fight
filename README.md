0777fb387438d422f2e7b81b378c9e9a8b487ca7
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-01-19 07:59:05 -0500

    user input functionality set up

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ both inputs have auto complete functionality
 once inputs are entered, user can select the movie they want
 information about the movie shows up
 between the two movies, which ever has higher values is highlighted accordingly
+big challenges in this project:
+- need to fetch data about movies (use omdbapi.com)
+we will be making two different request
+one to search the title of the movie
+second to get all the information about a specific movie
+- decide how the autocomplete widget will work
+- style the project better with css
--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello')
+// fetching data from the api
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com', {
+        params: {
+            apikey: '57a20f74',
+            s: searchTerm 
+        }
+    });
+    console.log(response.data)
+}
+
+const input = document.querySelector('input');
+
+// accessing user input
+let timeoutId;
+
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(()=>{
+        fetchData(event.target.value);
+    }, 1000)
+};
+
+input.addEventListener('input', onInput);
