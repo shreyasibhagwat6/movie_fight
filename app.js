e8812f8b85b1b88751b29de6d9d2f10971ef59a1
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-01-20 12:28:04 -0500

    displaying movie list

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // fetching data from the api
+
 const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com', {
         params: {
@@ -6,21 +7,27 @@ const fetchData = async (searchTerm) => {
             s: searchTerm 
         }
     });
-    console.log(response.data)
+    return response.data.Search;
 }
 
 const input = document.querySelector('input');
 
-// accessing user input
-let timeoutId;
+// accessing user input and making api request after timeout
 
-const onInput = event => {
-    if (timeoutId) {
-        clearTimeout(timeoutId);
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    console.log(movies);
+
+// display the movie list received
+
+    for(let movie of movies) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${movie.Poster}"/>
+        <h1>${movie.Title}</h1>
+        `;
+        document.querySelector('#target').appendChild(div);
     }
-    timeoutId = setTimeout(()=>{
-        fetchData(event.target.value);
-    }, 1000)
 };
 
-input.addEventListener('input', onInput);
+input.addEventListener('input', debounce(onInput), 500);
