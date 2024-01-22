97720b529b36c84cba8dab8e1c3de3ffe841bc58
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-01-22 09:34:53 -0500

    movie details rendering on page

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // fetching data from the api
 
 const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com', {
+    const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '57a20f74',
             s: searchTerm 
         }
     });
+
 // handling error when no such movie is found
+
     if(response.data.Error) {
         return[];
     }
@@ -33,13 +35,19 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
-    console.log(movies);
+
+    if(!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
 
 // display the movie list received
 
+    resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
     for(let movie of movies) {
         const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
         option.classList.add('dropdown-item');
         option.innerHTML = `
@@ -47,8 +55,75 @@ const onInput = async event => {
         ${movie.Title}
         `;
 
+// handling user clicking on a specific movie title
+
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        })
+
         resultsWrapper.appendChild(option);
     }
 };
 
 input.addEventListener('input', debounce(onInput), 500);
+
+// close the dropdown when clicked outside 
+
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+})
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '57a20f74',
+            i: movie.imdbID 
+        }
+    });
+    console.log(response.data);
+
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+    return `
+        <article class="media">
+            <figure class="media-left>
+                <p class="image">
+                    <img src="${movieDetail.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">BoxOffice</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+    `;
+}
